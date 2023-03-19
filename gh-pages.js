ed58a39c144a661a9ffcccf76a04830362f5c38e
GisangLee <masterkorea01@naver.com>
2023-03-19 13:38:05 +0900

    fix

--- gh-pages.js
+++ gh-pages.js
@@ -1,15 +1,17 @@
-const ghpages = require('gh-pages');
+import { publish } from 'gh-pages';
 
-ghpages.publish(
+publish(
     'public', // path to public directory
     {
-        branch: 'gh-pages',
+        branch: 'main',
         repo: 'https://github.com/GisangLee/GisangLee.github.io.git', // Update to point to your repository  
         user: {
             name: 'GisangLee', // update to use your name
             email: 'masterkorea01@naver.com' // Update to use your email
-        }
+        },
+        dotfiles: true
     },
+    
     () => {
         console.log('Deploy Complete!')
     }
